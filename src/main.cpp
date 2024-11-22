2ab41361618a196344b8ea94216324c82a414f66
Kayleb814 <kayleb_s@hotmail.com>
2024-11-22 15:30:54 -0800

    Averaging started not done

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,12 @@
 #define sendPhMessageFreq 4000 // frequency of sending LED intensity messages to the App
 #define updatePHvalues 1000    // delay time in milliseconds to update the pH value
 
+#define avgCount 100 // number of samples to average
+#define timeBetweenPhSamples 1000 // time between samples in milliseconds
+unsigned long startAvg = millis(); // start time for averaging
+float runningTotalPhReads = 0; // running total of pH reads
+float sumOfAvgPhReads = 0; // sum of the average
+
 #define NODE_ID 165                       // Node ID of the device
 #define SEND_PH_MESSAGE_ID 0x0A00         // 2560
 #define LOW_PH_ALARM_MESSAGE_ID 0x0A01    // 2561
@@ -47,7 +53,7 @@ uint64_t errorData0 = 0;
 
 float voltage;
 float phValue;
-float temperature = 20;
+float temperature = 20;     // temperature value updated from the CAN bus
 
 // PH sensor object
 DFRobot_PH DFRobot_PH_sensor;
@@ -60,7 +66,7 @@ NodeControllerCore core;
 void receive_message(uint8_t nodeID, uint16_t messageID, uint64_t data);
 
 // Function to update the pH value
-float updatePH();
+float avgPhValue();
 
 // Function to send the pH value to the CAN bus
 void sendPHMessage();
@@ -71,6 +77,7 @@ void setup()
 {
     Serial.begin(115200);
     DFRobot_PH_sensor.begin();
+    DFRobot_PH_sensor.calibration(voltage,temperature);           // calibration process by Serail CMD
 
     // Create the node controller core object
 
@@ -92,26 +99,26 @@ void setup()
 
 void loop()
 {
-    updatePH();
+    avgPhValue();
 
     sendPHMessage();
 }
 
 ////////////////////////////////////////////// put function definitions here:  /////////////////////////////////////////////////////////
 
-float updatePH()
+float avgPhValue()
 {
-    static unsigned long timepoint = millis();
-    if (millis() - timepoint > 1000U)
-    { // time interval: 1s
-        timepoint = millis();
-        // temperature = readTemperature();         // read your temperature sensor to execute temperature compensation
-        // voltage = analogRead(PH_PIN)/1024.0*5000;  // read the voltage
-        voltage = (analogReadMilliVolts(PH_PIN)/1000.0); // read the voltage
-        //phValue = random(0, 14);                       // random pH value for testing
-        phValue = DFRobot_PH_sensor.readPH(voltage, temperature); // convert voltage to pH with temperature compensation
-
-        // DFRobot_PH_sensor.calibration(voltage,temperature);           // calibration process by Serail CMD
+    for(int i=avgCount; i<avgCount; i++)
+    {
+    startAvg = millis();
+        if(millis() < (startAvg + timeBetweenPhSamples))
+        {
+            voltage = (analogReadMilliVolts(PH_PIN)/1000.0); // read the voltage
+            phValue = DFRobot_PH_sensor.readPH(voltage, temperature); // convert voltage to pH with temperature compensation
+            runningTotalPhReads =
+        }
+    }
+        sumOfAvgPhReads = (runningTotalPhReads / avgCount);
 
 #ifdef debuging
         
@@ -121,7 +128,6 @@ float updatePH()
         Serial.print("^C  pH:");
         Serial.println(phValue, 2);
 #endif
-    }
     delay(updatePHvalues);
     return phValue;
 }
